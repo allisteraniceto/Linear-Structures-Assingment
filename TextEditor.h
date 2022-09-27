fa23c1b2a84e1b57201402e1dc826356862cc4e4
allisteraniceto <allisteraniceto@gmail.com>
2022-09-26 22:11:49 -0700

    first commit

---
+++ TextEditor.h
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <string>
+using namespace std;
+
+//use each line as an element in the list 
+//using a list (vector) to easily access a line and make changes
+
+//HOW THE VECTOR IS BEING USED:    
+//read list into vector list
+//then
+//write list back onto the text file
+
+//rubric:
+//NEED: to use try, catch, throw (exception handling)
+//commands are CASE SENSITIVE!
+
+class TextEditor{
+private:
+    vector<string> lines; //store a list of strings for each line
+    int lineNum; //when user prompts to edit/view line, store in here
+    string fileName;
+    string line;
+    ifstream readFile;
+    ofstream writeFile;
+public:
+    TextEditor(); 
+    /*
+    default constructor
+    */
+    TextEditor(string); 
+    /*
+    overloaded constructor
+    */
+    void load();
+    /*
+    load .txt file function;
+    */
+    void write(string);
+    /*
+    write .txt file function;
+    */
+    void makeVec();
+    /*
+    -load text file
+    -insert individual line into each element of the vector list
+    */
+};
+
+TextEditor::TextEditor(){
+    this->lineNum=0;
+    this->fileName="file.txt";
+}
+TextEditor::TextEditor(string fileName){
+    this->fileName=fileName;
+}
+void TextEditor::write(string line){
+    writeFile.open(fileName);
+    if (!writeFile){
+        cout << "ERROR! WRONG FILE!!";
+    }
+    if (writeFile.is_open()){
+        writeFile << line << "\n";
+    }
+}
+void TextEditor::load(){
+    readFile.open(fileName);
+    if (!readFile){
+        cout << "ERROR! WRONG FILE!!";
+    }
+    getline(readFile, line);
+}
+void TextEditor::makeVec(){
+    readFile.open(fileName); //open .txt file
+
+}
---
+++ text_editor.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <sstream> //use sstream to extract indivual characters from line
+#include "TextEditor.h"
+using namespace std;
+
+void menu() {
+	cout << endl;
+	cout << "*--------------------------------------------------------*\n";
+	cout << "W [<filename>] - Writes the contents of the current \"buffer\" to a file. If no filename is given, then it uses the file name of the file that has been opened. If no file has been opened, then the editor will indicate to the user that a file name must be given.\n\n";
+	cout << "J <line number> - Jump to a line in the file. -1 will jump to the end of the file, 0 will jump to the beginning of the file\n\n";
+	cout << "I [<some text>] - Insert the text at the \"current\" line. If no text is given, will insert a blank line\n\n";
+	cout << "A [<some text>] - Insert the text after the \"current\" line. If no text is given, will insert a blank line\n\n";
+	cout << "L [<line number>] [<line number>] - Lists the contents of the buffer. If no line numbers are given, lists the contents of the entire buffer. If one line number is given, shows only that line. If 2 line numbers are given, lists all the lines between the numbers, including the lines at the start and end number.\n\n";
+	cout << "D [<line number>] [<line number>] - Deletes one or more lines. If no line numbers are given, deletes the current line. If 1 line number is given, deletes that line. If 2 line numbers are given, deletes the range of lines, inclusive of the start and end line numbers. The first line number given is the start, and the second is the end. Should check to see if the line numbers are in the appropriate order.";
+	cout << "Q - Quits the editor. If the buffer has been modified and not written, the close command should prompt the user and ask if they want to save changes before quitting.\n\n";
+	cout << "*--------------------------------------------------------*"<<endl;
+}
+
+int main(){
+    char command; //user inputs single character command 'J','A', etc.
+    string text;
+
+    TextEditor file;
+    menu();
+    
+    cin >> command;
+    getline(cin, text); //after getting command, get the rest of the line 
+    //use switch for different commands
+    switch(command){ //switch requires integers
+
+    case 'W':{ //write to a file
+        file.write(text);
+    } 
+    case 'J':{ //jump to a line
+    //need to change string text to an int for a line 
+    //format J <line number> 
+    cout << "Output the text after command: " << text << endl;
+    }
+    case 'I':{ //insert text AT current line
+
+    }
+    case 'A':{ //insert text AFTER current line
+
+    }
+    case 'L':{ //liss contents of the buffer
+
+    }
+    case 'D':{ //deletes one or more lines
+
+    }
+    case 'Q':{ //quits the editor
+
+    }
+    }
+    
+    //cout << "hello world" << endl;
+    //if (command == "W")
+    return 0;
+}
+
+//to comment highlighted text: shift \+ alt \+ a
